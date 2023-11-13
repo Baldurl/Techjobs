a40e8c9f0d4d347770fd10c4c22301d9790967b2
Jordan <102927194+Baldurl@users.noreply.github.com>
2023-11-13 12:00:25 -0300

    Geral

--- auth/permissoes.php
+++ auth/permissoes.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once __DIR__ . '/../src/database/conexao.php';
 require_once __DIR__ . '/../src/dao/usuariodao.php';
 
@@ -11,3 +10,4 @@ $usuario = $dao->getById($id);
 $perfil_id = $_SESSION['usuario']['perfil'];
 $dao = new UsuarioDAO();
 $perfil = $dao->getByPerfilId('perfil_id');
+
